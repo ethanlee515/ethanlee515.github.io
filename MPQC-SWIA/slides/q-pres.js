dd86ec270fe238ce9b93c20e4cf1dd96e3a2f97a
Ethan Lee <ethanlee515@gmail.com>
2021-07-28 22:01:38 +0800

    Implement visible-from attributes

--- MPQC-SWIA/slides/q-pres.js
+++ MPQC-SWIA/slides/q-pres.js
@@ -89,11 +89,25 @@ function drawLineBetweenParties(pi, pj, canvas, isArrow) {
 
 let currentSlide = 0;
 
+function updateSlideContentVisibility() {
+	let slideContents = $('.slide-content');
+	for(let i = 0; i < slideContents.length; ++i) {
+		let slideContent = slideContents.eq(i);
+		let visibleFrom = slideContent.data('visible-from');
+		if(visibleFrom != null) {
+			slideContent.css('visibility',
+				currentSlide >= visibleFrom ? 'visible' : 'hidden');
+		}
+	}
+}
+
 function main() {
+	updateSlideContentVisibility();
 	nextSlide();
 	$('body').keydown(e => {
 		if(e.key == "ArrowRight") {
 			currentSlide++;
+			updateSlideContentVisibility();
 			nextSlide();
 		}
 	});
