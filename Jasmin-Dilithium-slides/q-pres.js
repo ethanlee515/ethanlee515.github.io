8e8d805614f87b857e9d6e12cf7592f739ed0d9d
Yi Lee <ylee1228@umd.edu>
2021-11-17 17:21:25 -0500

    Add title page

---
+++ Jasmin-Dilithium-slides/q-pres.js
@@ -0,0 +1,26 @@
+let currentSlide = 0;
+
+function updateSlideContentVisibility() {
+	let slideContents = $('.slide-content');
+	for(let i = 0; i < slideContents.length; ++i) {
+		let slideContent = slideContents.eq(i);
+		let visibleFrom = slideContent.data('visible-from');
+		if(visibleFrom != null) {
+			slideContent.css('visibility',
+				currentSlide >= visibleFrom ? 'visible' : 'hidden');
+		}
+	}
+}
+
+function main() {
+	updateSlideContentVisibility();
+	nextSlide();
+	$('body').keydown(e => {
+		if(e.key == "ArrowRight") {
+			currentSlide++;
+			updateSlideContentVisibility();
+			nextSlide();
+		}
+	});
+}
+
