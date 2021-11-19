94729bac9d761b610ab74bfc6d8be0b434931ff8
Yi Lee <ylee1228@umd.edu>
2021-11-19 00:36:35 -0500

    Finish first draft of slides

--- Jasmin-Dilithium-slides/complaints.js
+++ Jasmin-Dilithium-slides/complaints.js
@@ -15,7 +15,7 @@ let complaints = [
 	"asmgen: not able to assemble address",
 	"asmgen: invalid instruction, check do not pass : MOV_32",
 	"asmgen: (compile_arg) not compatible asm_arg",
-	"error in “one-varmap” checker: modified expression",
+	'error in "one-varmap" checker: modified expression',
 	"Error: operand size mismatch for `movzb'",
 	"Stack smashing detected",
 	"And I thought segfaults were bad",
@@ -25,10 +25,35 @@ let complaints = [
 	"Syntax error in extracted EasyCrypt"
 ];
 
+let complaint_index = 0;
+
+function make_complaint(complaint) {
+	let div = $(`<div>${complaint}</div>`);
+	div.css('position', 'absolute');
+	div.css('width', '100%');
+	div.css('top', '100%');
+	div.css('display', 'flex');
+	div.css('justify-content', 'center');
+	$('body').append(div);
+	div.animate({'top': '-15%'}, 3000, 'linear');
+}
+
+function display_next_complaint() {
+	if(complaint_index < complaints.length) {
+		let complaint = complaints[complaint_index];
+		complaint_index++;
+		make_complaint(complaint);
+	}
+}
 
 function nextSlide() {
 	//Sorry. Had to get this off my chest.
 	switch(currentSlide) {
+		case 0: {
+			$('body').css('overflow', 'hidden');
+			setInterval(display_next_complaint, 150);
+			break;
+		}
 		case 2: {
 			location.href = "easycrypt.html";
 			break;
--- Jasmin-Dilithium-slides/jasmin.js
+++ Jasmin-Dilithium-slides/jasmin.js
@@ -1,15 +1,8 @@
 function nextSlide() {
 	switch(currentSlide) {
-		case 6: {
+		case 3: {
 			location.href = "complaints.html";
 			break;
 		}
 	}
 }
-
-
-/*
-https://www.google.com/chrome/static/images/chrome-logo.svg
-
-256.115.117.115
-*/
